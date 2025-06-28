f0b55a9d84ea6ed0598a652faac2a18b8a36097a
m-gamao <42097750+m-gamao@users.noreply.github.com>
2025-06-28 13:30:32 -0400

    added models and controllers

---
+++ app/models/message.rb
@@ -0,0 +1,19 @@
+class Message
+    include Mongoid::Document
+    include Mongoid::Timestamps
+  
+    field :body, type: String
+    field :session_id, type: String
+  
+    validates :body, presence: true
+    validates :session_id, presence: true
+  
+    def as_json(options = {})
+      {
+        id: id.to_s,
+        body: body,
+        created_at: created_at
+      }
+    end
+  end
+  
